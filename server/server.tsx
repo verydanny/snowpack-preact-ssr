6e00870f06b72d4f8391872463b404b772a93340
Daniil Veremchuk <daniel@verydanny.com>
2020-10-28 11:29:34 -0400

    clear out single module

--- server/server.tsx
+++ server/server.tsx
@@ -61,7 +61,7 @@ if (DEV) {
         }
       })
 
-      fg.sync(`${serverDir}/**/*`).forEach((file) => clearModule(file))
+      fg.sync(`${serverDir}/**/*`).forEach((file) => clearModule.single(file))
     }
   })
 }

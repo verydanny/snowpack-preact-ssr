c569208603d8c4cd3651398133b9a625388bed96
Daniil Veremchuk <daniel@verydanny.com>
2020-10-28 11:30:57 -0400

    comment

--- server/extensionShim.ts
+++ server/extensionShim.ts
@@ -1,3 +1,6 @@
 /* eslint-disable node/no-deprecated-api */
 
+// Create a noop for css, less, sass, etc. Just don't import
+// them. This unfortunately means no css modules SSR, but one
+// may probably enable it with babel.
 require.extensions['.css'] = () => {}
